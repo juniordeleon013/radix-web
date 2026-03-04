1a04a280557dfd9df11f84f0c475e14099913037
Jun De Leon <jun.emiliodiaz@gmail.com>
2026-03-03 20:59:57 -0400

    se añadio analitics de vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="es" className="scroll-smooth">
       <body className="bg-radix-secondary text-slate-800">
         {children}
+        <Analytics />
       </body>
     </html>
   );
